29c1b933bdc791687ce9d61cd9914ff1725e84fe
Sishir <sishir.bhattarai@outlook.com>
2020-12-21 23:28:18 -0700

    added route on html index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+
 const userRoutes = require('./commentRoutes');
 const userRoutes = require('./postRoutes');
 const userRoutes = require('./userRoutes');
 
+//telling router to use api routes
 router.use('/users', userRoutes);
 router.use('/users', postRoutes);
 router.use('/users', commentRoutes);
